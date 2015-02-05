f3849640dd9dbd0b059c3e6439ac2a594219ad30
Gisiona <gil-real@hotmail.com>
2015-02-04 22:26:18 -0200

    Adicionado a pagina de login

--- ONESOFT.EntregaOnline.Dominio/Entidades/UsuarioLogin.cs
+++ ONESOFT.EntregaOnline.Dominio/Entidades/UsuarioLogin.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ONESOFT.EntregaOnline.Dominio
 {
    public class UsuarioLogin
-    {
+    {       
         public int Id { get; set; }
         public Guid Hash { get; set; }
         public string Senha { get; set; }
